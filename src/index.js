9a5f9c3563c90babe8aefe8e5d6603c27dee45e7
akshaypandey44 <kumarakshaypandey@gmail.com>
2020-12-18 10:34:10 +0530

    check

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
-
+const {newsArticleModel} = reqire("./connector");
 const onePageArticleCount = 10
 
 
@@ -9,8 +9,15 @@ const onePageArticleCount = 10
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-
+app.get("/newsFeeds", async(req,rres)=>{
+   res.send( await newsArticleModel.find().skip(Number(req.query.offset || 0)).limit(san(req.query.limit, 10)));
+})
+const san = (value,defaultValue)=>{
+    if(value == null || value == undefined || Number(value))
+        return defaultValue;
+    else
+        return Number(value);
+}
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;
