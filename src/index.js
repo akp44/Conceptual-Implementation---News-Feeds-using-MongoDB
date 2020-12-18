0db1e5a84e297d18067df0046547018e96df7f84
akshay pandey <kumarakshaypandey@gmail.com>
2020-12-18 12:05:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
-const { newsArticleModel } = reqire("./connector");
+const { newsArticleModel } = require('./connector');
 const onePageArticleCount = 10
 
 
